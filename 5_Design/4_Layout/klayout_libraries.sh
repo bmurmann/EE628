81658481f94cb85eadc47094a4365e6fc2b65c55
bmurmann <bmurmann@gmail.com>
2024-04-27 06:29:36 -1000

    Updated top layout

---
+++ 5_Design/4_Layout/klayout_libraries.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script places softlinks into /foss/designs/.klayout/libraries
+
+ln -s /foss/designs/EE628/5_Design/4_Layout/sg13g2_io_bm.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/bondpad_80x80.gds /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/padring.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/sg13g2_pr.gds /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/sg13g2_stdcell.gds /foss/designs/.klayout/libraries
+
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 1/Team1.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 2/Team2.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 3/Team3.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 4/Team4.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 5/Team5.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team6.oas /foss/designs/.klayout/libraries
+
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team1_logo.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team2_logo.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team3_logo.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team4_logo.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team5_logo.oas /foss/designs/.klayout/libraries
+ln -s /foss/designs/EE628/5_Design/4_Layout/Team\ 6/Team6_logo.oas /foss/designs/.klayout/libraries
